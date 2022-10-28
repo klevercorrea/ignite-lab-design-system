cc8ff784e5a62f500da0a9608d0219e5e1db6a59
Klever Correa da Silveira <klevercorrea@icloud.com>
2022-10-27 22:03:37 -0300

    add jest to storybook

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 import { SignIn } from "./SignIn";
 
 export default {
@@ -8,4 +10,10 @@ export default {
   argTypes: {},
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: ({canvasElement}) => {
+    const canvas = within(canvasElement)
+
+    userEvent.type()
+  }
+};
